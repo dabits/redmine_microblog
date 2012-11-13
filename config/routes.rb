32e0ac5e3aed1a60eaea1c542323c402134a8c90
Hiroki Tanaka <tanaka@dabits.net>
2012-11-13 21:35:30 +0900

    only for redmine-1.x

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
-if Rails.version < '3.0'
-  ActionController::Routing::Routes.draw do |map|
-    map.resources :microblogs, :controller => :microblogs, :collection => [:index_project, :more_recent, :more_history]
-    map.connect 'projects/:project_id/microblogs/:action', :controller => :microblogs, :action => :index_project
-  end
-else # Rails 3
-  RedmineApp::Application.routes.draw do
-    match 'microblogs/:action', :controller => 'microblogs'
-  end
+ActionController::Routing::Routes.draw do |map|
+  map.resources :microblogs, :controller => :microblogs, :collection => [:index_project, :more_recent, :more_history]
+  map.connect 'projects/:project_id/microblogs/:action', :controller => :microblogs, :action => :index_project
 end
